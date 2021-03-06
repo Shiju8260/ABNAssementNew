bd3517460fa3043468b6b9408360249c56ffad8c
Shiju8260 <shijub8260@gmail.com>
2021-03-06 18:12:21 +0530

    Assessment Completion

--- app/src/main/java/com/abn/amroassessment/MainActivity.kt
+++ app/src/main/java/com/abn/amroassessment/MainActivity.kt
@@ -1,11 +1,8 @@
 package com.abn.assessment
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import com.abn.amroassessment.R
-import com.abn.amroassessment.database.VenueRoomDatabase
-import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,7 +10,4 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
     }
-}
-fun View.snack(message: String, duration: Int = Snackbar.LENGTH_LONG) {
-    Snackbar.make(this, message, duration).show()
 }
